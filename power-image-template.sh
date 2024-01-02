bc6316a216b885010680a44c2008e8b38f219b2b
Jose Guerra <jose.guerra.m@uni.pe>
2024-01-02 15:20:20 -0500

    COS automation added

--- power-image-template.sh
+++ power-image-template.sh
@@ -7,6 +7,8 @@ fi
 # login validation
 ibmcloud login --apikey $IBM_CLOUD_API_KEY
 
+image_name='test-cli-'$(date +%m-%d-%Y)
+
 # CSM-Power-test Workspace target
 service_list_output=$(ibmcloud pi service-list)
 
@@ -19,18 +21,19 @@ instances_list_output=$(ibmcloud pi instances)
 
 instance_crn=$(echo "$instances_list_output" | awk '/lpar-bkp-ibmi/ {print $1}')
 
-image_list_output=$(ibmcloud pi images) 
+#image_list_output=$(ibmcloud pi images) 
 
 # Check if test-cli image exists
-if echo "$image_list_output" | grep -q 'test-cli'; then 
-	image_crn=$(echo "$image_list_output" | awk '/test-cli/ {print $1}') 
-	echo "Eliminando la imagen existente..." 
-	ibmcloud pi image-delete "$image_crn" 
-	sleep 15
-else
-    echo "No se encontró la imagen 'test-cli', continuando."
-fi 
+#if echo "$image_list_output" | grep -q 'test-cli'; then 
+#	image_crn=$(echo "$image_list_output" | awk '/test-cli/ {print $1}') 
+#	echo "Eliminando la imagen existente..." 
+#	ibmcloud pi image-delete "$image_crn" 
+#	sleep 15
+#else
+#    echo "No se encontró la imagen 'test-cli', continuando."
+#fi 
 # Capture te image
-ibmcloud pi instance-capture "$instance_crn" --destination image-catalog --name test-cli --job
+
+ibmcloud pi instance-capture "$instance_crn" --destination cloud-storage --access-key $COS_ACCESS_KEY --secret-key $COS_SECRET_KEY --region us-east --image-path power-vs-instance-bucket-images --name "$image_name" --job
 sleep 5
 ibmcloud logout
