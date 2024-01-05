4c5f52f668fdfa86f40b491fa1a1fa5d164a5422
Jose Guerra <jose.guerra.m@uni.pe>
2024-01-05 18:33:25 -0500

    Dockerfile updated & env added

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ubuntu:latest
 RUN apt-get update && apt-get install -y curl ca-certificates
 RUN curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
+RUN ibmcloud plugin install power-iaas
 COPY power-image-template.sh /app/
 WORKDIR /app
 RUN chmod +x power-image-template.sh 
--- power-image-template.sh
+++ power-image-template.sh
@@ -7,32 +7,21 @@ fi
 # login validation
 ibmcloud login --apikey $IBM_CLOUD_API_KEY
 
-image_name='test-cli-'$(date +%m-%d-%Y)
+image_name='image-template-'$(date +%m-%d-%Y)
 
-# CSM-Power-test Workspace target
+# Power Workspace target
 service_list_output=$(ibmcloud pi service-list)
 
-service_crn=$(echo "$service_list_output" | awk '/CSM-Power-test/ {print $1}')
+service_crn=$(echo "$service_list_output" | awk -v workspace="$IBM_POWER_WORKSPACE_NAME" '$0 ~ workspace {print $1}')
 
 ibmcloud pi service-target "$service_crn"
 
-# lpar-bkp-ibmi instance 
+# Power LPAR instance 
 instances_list_output=$(ibmcloud pi instances)
 
-instance_crn=$(echo "$instances_list_output" | awk '/lpar-bkp-ibmi/ {print $1}')
+instance_crn=$(echo "$instances_list_output" | awk -v workspace="$IBM_POWER_INSTANCE_NAME" '$0 ~ workspace {print $1}')
 
-#image_list_output=$(ibmcloud pi images) 
-
-# Check if test-cli image exists
-#if echo "$image_list_output" | grep -q 'test-cli'; then 
-#	image_crn=$(echo "$image_list_output" | awk '/test-cli/ {print $1}') 
-#	echo "Eliminando la imagen existente..." 
-#	ibmcloud pi image-delete "$image_crn" 
-#	sleep 15
-#else
-#    echo "No se encontró la imagen 'test-cli', continuando."
-#fi 
-# Capture te image
+#Capture image in IBM COS
 
 ibmcloud pi instance-capture "$instance_crn" --destination cloud-storage --access-key $COS_ACCESS_KEY --secret-key $COS_SECRET_KEY --region us-east --image-path power-vs-instance-bucket-images --name "$image_name" --job
 sleep 5
